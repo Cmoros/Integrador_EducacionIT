9096d8ea23d4138b3e9075938ff48cb7bff22370
Cesar Moros <rdcesar442@gmail.com>
2022-09-13 03:37:28 -0300

    footer para movil terminado

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Header
+const mainHeader = document.querySelector('.main-header');
+const searchFormToggle = document.querySelector('#main-nav-search-form-toggle');
+
+
+
+class ResponsiveDesign {
+    constructor() {
+        window.addEventListener('resize', e => {
+
+        })
+    }
+
+    
+}
+
+const responsivePage = new ResponsiveDesign();
