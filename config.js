d9600b5626ce97936381f0edd77bc265c2d46e53
Cesar Moros <rdcesar442@gmail.com>
2022-12-25 19:32:51 -0300

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,6 @@ const PERSISTENCE_TYPE = {
 export default {
   PORT: process.env.PORT || 8081,
   PERSISTENCE_TYPE: PERSISTENCE_TYPE.TYPE_MONGODB,
-  // MONGODB_CONNECTION_STR: 'mongodb://localhost/ecommerce',
-  // MONGODB_CONNECTION_STR: "mongodb://localhost:27017/ecommerce",
-  // MONGODB_CONNECTION_STR: 'mongodb+srv://juanromeroclases:HolaHola123@cluster0.5wxsavc.mongodb.net/ecommerce?retryWrites=true&w=majority',
   MONGODB_CONNECTION_STR:
     "mongodb+srv://cmoros:ecommerce123@jcosmicadb.zrjnr6g.mongodb.net/ecommerce?retryWrites=true&w=majority",
   MONGODB_TIMEOUT: 2000,
